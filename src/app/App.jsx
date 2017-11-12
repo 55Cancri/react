c93b9d840bb1a5fbc7c1e1ae3e5dbfcb0685f8a3
Eric Morrison <ericmorrison@Erics-MacBook-Pro.local>
2017-11-11 21:13:26 -0500

    refined starting point

---
+++ src/app/App.jsx
@@ -0,0 +1,18 @@
+// [✓]
+
+import React, { Component } from 'react'
+import './App.sass'
+
+class App extends Component {
+  render() {
+    return (
+
+      // all components inside this div
+      <div className='App'>
+        <h1>React Web App</h1>
+      </div>
+    )
+  }
+}
+
+export default App
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,28 @@
-var path = require('path');
+const path = require('path')
 
 module.exports = {
 
-    entry: path.resolve(__dirname, 'src') + '/app/index.jsx',
-    output: {
-        path: path.resolve(__dirname, 'dist') + '/app',
-        filename: 'bundle.js',
-        publicPath: '/app/'
-    },
-    module: {
+  entry: path.resolve(__dirname, 'src') + '/app/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist') + '/app',
+    filename: 'bundle.js',
+    publicPath: '/app/'
+  },
+  module: {
 
-        loaders: [
-            {
-                test: /\.jsx$/,
-                include: path.resolve(__dirname, 'src'),
-                loader: 'babel-loader',
-                query: {
-                    presets: ['react', 'es2015']
-                }
-            },
-            {
-                test: /\.sass$/,
-                loader: 'style-loader!css-loader!sass-loader'
-            }
-        ]
-    },
-};
+    loaders: [
+      {
+        test: /\.jsx$/,
+        include: path.resolve(__dirname, 'src'),
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      },
+      {
+        test: /\.sass$/,
+        loader: 'style-loader!css-loader!sass-loader'
+      }
+    ]
+  }
+}
